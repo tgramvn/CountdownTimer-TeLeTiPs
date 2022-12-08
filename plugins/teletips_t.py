dd4acc5415c0b853f25cc30e1c83196ca53f7e2e
tgramvn <109977670+tgramvn@users.noreply.github.com>
2022-12-09 04:00:44 +0700

    Update teletips_t.py

--- plugins/teletips_t.py
+++ plugins/teletips_t.py
@@ -5,30 +5,16 @@
 # Changing the code is not allowed! Read GNU AFFERO GENERAL PUBLIC LICENSE: https://github.com/teletips/CountdownTimer-TeLeTiPs/blob/main/LICENSE
                        
   
-START_TEXT = 'Heya,\nMy name is **Countdown Timer** ⏰\n\nI can countdown to your important events in **any group chat**. 🎉\nUse me to keep track of how much time is left for the event ⏳\n\nHit **❓HELP** to find out more about how to use me.'
+START_TEXT = 'Automatic delete your group!'
 
 GROUP_TEXT="""
 👥 | **HELP & SUPPORT**
-<i>Feel free to report any issues regarding this service.</i>
+<i>Đã thêm vào nhóm, vui lòng cài đặt.</i>
 """
 
-HELP_TEXT = """
-🚀 | **GET STARTED**
-<i>Add your bot to the group.</i>
-👮🏻‍♂️ | **ADMIN PERMISSIONS**
-<i>Don't forget to make your bot admin of the group.</i>
-🕹 | **COMMANDS**
-✦ **set**
-<i>Send the command below in correct format to the group.</i>
-• <code>/set seconds "event"</code>
-**example**
-• <code>/set 10 "10 seconds countdown"</code>
-✦ **stopc**
-<i>You can use <code>/stopc</code> command to stop all countdowns.</i> 
-⚠️ | **WARNING**
-<i>Do not do more than 2 countdowns at the same time using the same bot.</i>
+HELP_TEXT = """No help
 """
 
-TUTORIAL_TEXT = "💡 | Click the **🎥 Video** button to see the way of creating this bot for your group easily."
+TUTORIAL_TEXT = "No."
 
 #Copyright ©️ 2021 TeLe TiPs. All Rights Reserved.
